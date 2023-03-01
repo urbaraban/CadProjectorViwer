c0c88538f8777cc026b916a23fa658bcaeef456d
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2023-03-01 09:59:58 +0700

    add IpCam window

--- MonchaCadViewer/Dialogs/IpCamDialog.xaml.cs
+++ MonchaCadViewer/Dialogs/IpCamDialog.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using RtspClientSharp;
+using RtspClientSharp.RawFrames.Video;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,8 +28,12 @@ namespace CadProjectorViewer.Dialogs
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            var serverUri = new Uri("rtsp://192.168.1.77:554/ucast/11");
+            var credentials = new NetworkCredential("admin", "123456");
+            var connectionParameters = new ConnectionParameters(serverUri, credentials);
+            connectionParameters.RtpTransport = RtpTransportProtocol.TCP;
 
         }
     }

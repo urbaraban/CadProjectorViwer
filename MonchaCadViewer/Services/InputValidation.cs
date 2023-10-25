350c8e5d9545168eab314b5b113e121c0bbe6442
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2023-10-25 21:28:18 +0700

    fix numeric input

--- MonchaCadViewer/Services/InputValidation.cs
+++ MonchaCadViewer/Services/InputValidation.cs
@@ -7,7 +7,7 @@ namespace CadProjectorViewer.Services
     {
         public static void NumberPerDotValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9.]+");
+            Regex regex = new Regex("[^0-9,]+");
             e.Handled = regex.IsMatch(e.Text);
         }
     }

fc02220846a86ac0f47d93e2f3548c066905b6ce
Шадрунов Егор Дмитриевич <urbaraban@outlook.com>
2024-03-03 14:09:14 +0700

    fix add tanh gradient

--- MonchaSDKTests/UnitTest1.cs
+++ MonchaSDKTests/UnitTest1.cs
@@ -1,16 +1,20 @@
-﻿using CadProjectorViewer.ViewModel;
+﻿using CadProjectorSDK.Render;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace MonchaSDKTests
 {
     [TestClass]
     public class UnitTest1
     {
+
+
         [TestMethod]
-        public void SerializiableTest()
+        public void TestLine()
         {
-            AppMainModel model = new AppMainModel();
-            model.SaveSceneCommand.Execute(null);
+            var rnd = new Random();
+            var line = new VectorLine(rnd.Next(), 0, rnd.Next(), 0, false);
+            Console.WriteLine(line);
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using CadProjectorSDK.Render;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LineTestMesth_0()
+        {
+            var rnd = new Random();
+            var line = new VectorLine(rnd.Next() % 1000 - 500, 0, rnd.Next() % 1000 - 500, 0, true);
+            Debug.WriteLine(line.ToString());
+
+            var lines = line.SplitGradient(8, 3);
+
+            Debug.WriteLine(lines.P1.ToString());
+
+            foreach(var item in lines)
+            {
+                Debug.WriteLine(item.P2.ToString());
+            }
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void LineTestMesth_1()
+        {
+            var rnd = new Random();
+            var line = new VectorLine(79, 0, 54, 0, true);
+            Debug.WriteLine(line.ToString());
+            var lines = line.SplitGradient(8, 1);
+            Debug.WriteLine(lines.P1.ToString());
+            foreach (var item in lines)
+            {
+                Debug.WriteLine(item.P2.ToString());
+            }
+
+            Assert.IsTrue(true);
+        }
+
+    }
+}
